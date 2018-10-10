9a8277b4f953a6aaa43bcdf67fee6087e10fe4fe
yangk <614044401@qq.com>
2018-10-10 16:45:41 +0800

    添加验证

---
+++ src/utils/util.js
@@ -0,0 +1,36 @@
+/**
+ * 工具函数
+ */
+// 验证是否是手机号码
+function vailPhone(number) {
+    let flag = true
+    let myreg = /^(((13[0-9]{1})|(14[0-9]{1})|(17[0]{1})|(15[0-3]{1})|(15[5-9]{1})|(18[0-9]{1}))+\d{8})$/
+    if (number.length !== 11 || !myreg.test(number)) {
+        flag = false
+    }
+    return flag
+}
+
+Date.prototype.format = function (format) {
+    var args = {
+        "M+": this.getMonth() + 1,
+        "d+": this.getDate(),
+        "h+": this.getHours(),
+        "m+": this.getMinutes(),
+        "s+": this.getSeconds(),
+        "q+": Math.floor((this.getMonth() + 3) / 3),  //quarter
+        "S": this.getMilliseconds()
+    };
+    if (/(y+)/.test(format))
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var i in args) {
+        var n = args[i];
+        if (new RegExp("(" + i + ")").test(format))
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? n : ("00" + n).substr(("" + n).length));
+    }
+    return format;
+};
+
+module.exports = {
+    vailPhone
+}
